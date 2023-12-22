7ee6479ce753e374f27e47d8dc681faefec2152e
Sysiphos <l.oberer@gmx.net>
2023-12-22 20:21:23 +0100

    entrypoint.sh: inverted tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,42 +5,44 @@ set -o pipefail
 
 
 # Check if the cron environmental variable is set
-if [ -n $CRON_SCHEDULE ]; then
+if [ -z "$CRON_SCHEDULE" ]; then
+    echo "Using default cron schedule"
+    # Set the default cron schedule
+    echo "0 0 * * * /request_cert.sh" | crontab -
+else
     echo "Using custom cron schedule: $CRON_SCHEDULE"
     
     # Copy the custom cron schedule into the crontab file
     echo "$CRON_SCHEDULE /request_cert.sh" | crontab -
-else
-    echo "Using default cron schedule"
-    # Set the default cron schedule
-    echo "0 0 * * * /request_cert.sh" | crontab -
 fi
 
-if [ -n $DEHYDRATED_CA ]; then
+if [ -z "$DEHYDRATED_CA" ]; then
+    echo "Using default CA: letsencrypt"
+else
     echo "Using custom CA: $DEHYDRATED_CA"
     # Copy the custom CA into the config file
     echo "CA=$DEHYDRATED_CA" >> $WORK_PATH/config
-else
-    echo "Using default CA: letsencrypt"
+    
 fi
 
-if [ -n $DUCKDNS_TOKEN ]; then
-    echo "Using DuckDNS token $DUCKDNS_TOKEN"
-else
+if [ -z "$DUCKDNS_TOKEN" ]; then
     echo "DUCKDNS_TOKEN is not defined. Please set the token."
     exit 1
+else
+    echo "Using DuckDNS token $DUCKDNS_TOKEN"
 fi
 
-if [ -n $MAIN_DOMAIN ]; then
+if [ -z "$MAIN_DOMAIN" ]; then
+    echo "DOMAINS is not defined. Please set the domains."
+    exit 1
+else
     echo "Using domains $MAIN_DOMAIN"
     # Copy the custom domains into the domains.txt file
     echo "$MAIN_DOMAIN" >> $WORK_PATH/domains.txt
-else
-    echo "DOMAINS is not defined. Please set the domains."
-    exit 1
 fi
 
-if [ -n $SUBDOMAINS ]; then
+if [ -z "$SUBDOMAINS" ]; then
+else
     arr=( $SUBDOMAINS )
     echo "Using subdomains $SUBDOMAINS"
     # Copy the custom subdomains into the domains.txt file
